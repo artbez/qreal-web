50e5de658d5729c3913fecc33f5efb7d358203c8
Artemii Bezguzikov <artemii.bezguzikov@gmail.com>
2016-03-04 00:52:54 +0300

    prepare for showing connection status

--- web/Web/authorization/js/app.js
+++ web/Web/authorization/js/app.js
@@ -1,5 +1,30 @@
 'use strict';
 
+angular.module('myApp.view1', ['ngRoute'])
+
+    .config(['$routeProvider', function($routeProvider) {
+      $routeProvider.when('/view1', {
+        templateUrl: 'view1/view1.html',
+        controller: 'View1Ctrl'
+      });
+    }])
+
+    .controller('View1Ctrl', [function() {
+
+    }]);
+
+angular.module('myApp.view2', ['ngRoute'])
+
+    .config(['$routeProvider', function($routeProvider) {
+      $routeProvider.when('/view2', {
+        templateUrl: 'view2/view2.html',
+        controller: 'View2Ctrl'
+      });
+    }])
+
+    .controller('View2Ctrl', [function() {
+
+    }]);
 
 // Declare app level module which depends on filters, and services
 angular.module('myApp', [
@@ -7,7 +32,9 @@ angular.module('myApp', [
   'myApp.filters',
   'myApp.services',
   'myApp.directives',
-  'myApp.controllers'
+  'myApp.controllers',
+  'myApp.view1',
+  'myApp.view2'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
